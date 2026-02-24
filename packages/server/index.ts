5e117c7b547361cb7bc92dddea215667de72f4a1
Juan Sebastián Ontiveros Gonzales <sebas.onti94@gmail.com>
2026-02-23 21:44:23 -0600

    Connect frontend to server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,8 +3,10 @@ import { Hono } from "hono";
 const app = new Hono();
 app.get("/", (c) => {
   console.log(process.env.CLAUDE_API_KEY);
-  return c.text("Hello Bun!!!");
+  return c.json({ message: "Hello Bun!!!" });
 });
+
+app.get("/api/hello", (c) => c.json({ message: "Hello Bun from Hono!" }));
 
 export default {
   port: 3001,
